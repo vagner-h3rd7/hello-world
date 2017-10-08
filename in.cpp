46544a18e5745eb88225b30946b8a89f44434f2b
Vagner <vhrf37@gmail.com>
2017-10-07 23:35:45 -0300

    Update in.cpp

--- in.cpp
+++ in.cpp
@@ -1,6 +1,7 @@
-#include <stdio.h>
-int main(void) {
-	printf("Ol\xA0 Mundo!\n");
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Ol\xA0 Mundo!" << '\n';
 getchar();
 return 0;
 }
