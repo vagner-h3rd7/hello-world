1dc19ad9953dd0e969bd27325924958862ab2ec4
Vagner H <vhrf37@gmail.com>
2018-08-28 23:46:35 -0300

    Create iN.cs

---
+++ iN.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iN{
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        
+    }
+}
